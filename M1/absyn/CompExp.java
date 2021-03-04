45787083bceae363dd890c6ad55494c1eb11dc3d
Jonah Stegman <jstegman@uoguelph.ca>
2021-03-04 15:08:11 -0500

    removed useless code

--- M1/absyn/CompExp.java
+++ M1/absyn/CompExp.java
@@ -1,22 +1,15 @@
 package absyn;
 
 public class CompExp extends Exp {
-    public final static int COMP = 0;
-    public final static int LOCDEC = 1;
-    public final static int STMTLIST = 2;
-    public final static int ARGLIST = 3;
-    public final static int EXPS = 4;
 
     public Exp first;
     public Exp second;
-    public int stmt;
 
-    public CompExp(int row, int col, Exp first, Exp second, int stmt) {
+    public CompExp(int row, int col, Exp first, Exp second) {
         this.row = row;
         this.col = col;
         this.first = first;
         this.second = second;
-        this.stmt = stmt;
     }
 
     public void accept(AbsynVisitor visitor, int level) {
